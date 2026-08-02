b22cdafab17dc5e9693bda36b8a178b9967c851a
SiyaPrince <nkosiprince02@gmail.com>
2026-08-02 19:28:27 +0200

    Initial commit, with the initial file structure setup.

---
+++ run_to_do_list.py
@@ -0,0 +1,15 @@
+# Create empty tasks list
+
+# Display welcome message
+
+# Repeat:
+#     Display menu
+#     Ask for choice
+
+#     Add
+#     View
+#     Search
+#     Update
+#     Complete
+#     Delete
+#     Exit
