7f25470f1f5c3981007a43e06717ef4bbb4d4b95
SiyaPrince <nkosiprince02@gmail.com>
2026-08-03 21:25:10 +0200

    search function done, also with a reusable helper function display found tasks

---
+++ core_operations/search_task.py
@@ -0,0 +1,12 @@
+from support_operations.display_task_details import display_task_details
+
+# Search for tasks based on a keyword
+def search_task(tasks):
+    searchd_keyword = input("Enter a keyword to search for tasks: ").strip().lower()
+
+    for task in tasks:
+        if searchd_keyword in task["title"].lower() or searchd_keyword in task["description"].lower():
+            print(f"Task found: {display_task_details(task)}")
+            return
+        else:
+            print(f"No task found with the keyword '{searchd_keyword}'.")
