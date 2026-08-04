cf25b3eb249a616e7e4a84c891df6de3d492d6a8
SiyaPrince <nkosiprince02@gmail.com>
2026-08-04 11:30:40 +0200

    view tasks fixed

--- core_operations/view_tasks.py
+++ core_operations/view_tasks.py
@@ -3,8 +3,10 @@ from support_operations.display_task_details import display_task_details
 # View the list of tasks
 def view_tasks(tasks):
     if not tasks:
-        print("No tasks available.")
-    else:
-        print("\n The list of tasks:")
-        for task in tasks:
-            display_task_details(task)
+        print("\nNo tasks available.")
+
+    print("\nList of tasks:")
+
+    for index, task in enumerate(tasks, start=1):
+        print(f"\nTask {index}")
+        display_task_details(task)
